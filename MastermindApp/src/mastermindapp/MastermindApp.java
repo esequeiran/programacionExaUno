b08abba0a23eea73b1e9ef2b2b06816fdb967ebc
esequeiran <esequeiran@ucenfotec.ac.cr>
2020-10-16 21:57:32 -0600

    Subir carpetas con ambos proyectos

---
+++ MastermindApp/src/mastermindapp/MastermindApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mastermindapp;
+
+/**
+ *
+ * @author Evese
+ */
+public class MastermindApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       MyFormMastermind myFormMastermind = new MyFormMastermind();
+       myFormMastermind.setVisible(true);
+    }
+    
+}
